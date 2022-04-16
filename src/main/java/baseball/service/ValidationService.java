24754145e086095e50400df1c0eaf88149675f77
dev-msj <msj944@naver.com>
2022-04-17 05:32:58 +0900

    feat(ValidationService): change function validation service interface
    Changed function name
    - validateBallNumber -> validateBallNumberLength
    Added validateDuplicateBallNumber function

--- src/main/java/baseball/service/ValidationService.java
+++ src/main/java/baseball/service/ValidationService.java
@@ -3,6 +3,7 @@ package baseball.service;
 import baseball.model.Ball;
 
 public interface ValidationService {
-    public boolean validateBallNumber(final Ball ball);
+    public boolean validateBallNumberLength(final Ball ball);
+    public boolean validateDuplicateBallNumber(final Ball ball);
     public boolean validateFinishNumber(final int finishNumber);
 }
