ca04f862b9c71ab4e633ff1a7642153d6437f0a9
dev-msj <msj944@naver.com>
2022-04-18 09:39:23 +0900

    feat(service): change service
    Changed service interface
    Added zero, type validation function in ValidationService
    Added createGameNumber function in ComputerService

--- src/main/java/baseball/service/ComputerService.java
+++ src/main/java/baseball/service/ComputerService.java
@@ -4,5 +4,6 @@ import baseball.model.Ball;
 import baseball.model.Hint;
 
 public interface ComputerService {
+    public void createGameNumber();
     public Hint compare(final Ball playerBall);
 }
--- src/main/java/baseball/service/ValidationService.java
+++ src/main/java/baseball/service/ValidationService.java
@@ -3,7 +3,9 @@ package baseball.service;
 import baseball.model.Ball;
 
 public interface ValidationService {
-    public boolean validateBallNumberLength(final Ball ball);
-    public boolean validateDuplicateBallNumber(final Ball ball);
-    public boolean validateFinishNumber(final int finishNumber);
+    public void validateBallNumberLength(final String ball) throws IllegalArgumentException;
+    public void validateBallNumberZero(final String ball) throws IllegalArgumentException;
+    public void validateBallNumberType(final String ball) throws IllegalArgumentException;
+    public void validateDuplicateBallNumber(final Ball ball) throws IllegalArgumentException;
+    public void validateFinishNumber(final String finishNumber) throws IllegalArgumentException;
 }
